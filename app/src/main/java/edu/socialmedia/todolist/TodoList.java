f62f51cc0abe79fd86f3b9ba562f31437340e571
grey122 <nonsoedu14@gmail.com>
2019-10-28 18:54:30 +0300

    Initial commit

---
+++ app/src/main/java/edu/socialmedia/todolist/TodoList.java
@@ -0,0 +1,14 @@
+package edu.socialmedia.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodoList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_TodoList);
+    }
+}
